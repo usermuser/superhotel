874ef9f495815783f24f443983690abcb52f194a
Sergio Burik <sergioburik@gmail.com>
2019-06-15 11:04:01 +0300

    Update urls.py

--- superhotel/superhotel/urls.py
+++ superhotel/superhotel/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from mainapp.view import mainapp
+from mainapp.views import mainapp
 
 
 urlpatterns = [
