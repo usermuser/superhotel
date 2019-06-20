30981eb4f34943218855cbd2f94c3ee507c2d3b5
Sergio Burik <sergioburik@gmail.com>
2019-06-20 21:15:41 +0300

    work with foms.py

---
+++ superhotel/mainapp/forms.py
@@ -0,0 +1,67 @@
+from django import forms
+
+class HeaderPhotoForm(forms.Form):
+    photos = forms.FileField()
+
+class NameOfHotelForm(forms.Form):
+    name_of_hotel = forms.CharField(max_length=100)
+
+class StarsForm(forms.Form):
+    STARS_CHOICES = (
+        (1, _("1 star hotel")),
+        (2, _("2 star hotel")),
+        (3, _("3 star hotel")),
+        (4, _("4 star hotel")),
+        (5, _("5 star hotel"))
+    )
+    stars = forms.ChoiceField(choices=STARS_CHOICES, label="", initial='', required=True)
+
+class NameOfHotelInfoForm(forms.Form):
+    info = forms.CharField(max_length=6000)
+
+class TelephoneNumberForm(forms.Form):
+    number = forms.IntegerField(required=True)
+
+class AddressForm(forms.Form):
+    address = CharField(max_length=100)
+
+class LocalityForm(forms.Form):
+    locality = CharField(max_length=200)
+
+class RegionForm(forms.Form):
+    region = CharField(max_length=200)
+
+class PostalCodeForm(forms.Form):
+    postalCode = forms.IntegerField(required=True)
+
+class MapLinkForm(forms.Form):
+    link = forms.CharField(max_length=2000)
+
+class UploadPhotoInfoForm(forms.Form):
+    # for x in range(6):
+    #     photo+x = forms.FileField
+    photo1 = forms.FileField()
+    photo2 = forms.FileField()
+    photo3 = forms.FileField()
+    photo4 = forms.FileField()
+    photo5 = forms.FileField()
+    photo6 = forms.FileField()
+
+class FeatureForm(forms.Form):
+    feature1 = CharField(max_length=100)
+    feature2 = CharField(max_length=100)
+    feature3 = CharField(max_length=100)
+    feature4 = CharField(max_length=100)
+    feature5 = CharField(max_length=100)
+    feature6 = CharField(max_length=100)
+    feature7 = CharField(max_length=100)
+    feature8 = CharField(max_length=100)
+
+class TermsAndConditionsForm(forms.Form):
+    terms = forms.CharField(max_length=3000)
+
+
+
+
+
+
