676443e00ff22a8c2f8a08ae60598b7593d34db1
Sergio Burik <sergioburik@gmail.com>
2019-06-21 19:41:14 +0300

    в

--- superhotel/mainapp/views.py
+++ superhotel/mainapp/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404, HttpResponseRedirect
-from django import forms
+from mainapp.forms import HeaderPhotoForm, NameOfHotelForm, StarsForm, NameOfHotelInfoForm
+from mainapp.forms import TelephoneNumberForm, AddressForm, LocalityForm, RegionForm, PostalCodeForm
+from mainapp.forms import MapLinkForm, UploadPhotoInfoForm, LongDescriptionOfHotel, FeatureForm, TermsAndConditionsForm
 # Create your views here.
 def dataFromInputBooking(request):
     headerPhoto = HeaderPhotoForm(request.POST)
     nameOfHotel_form = NameOfHotelForm(request.POST)
     starsForm = StarsForm(request.POST)
-    nameOfHotelInfo = NameOfHotelInfoForm(request.Post)
+    nameOfHotelInfo = NameOfHotelInfoForm(request.POST)
     telephoneNumberForm = TelephoneNumberForm(request.POST)
     addressForm = AddressForm(request.POST)
     localityForm = LocalityForm(request.POST)
