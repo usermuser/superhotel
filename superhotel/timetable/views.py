239cf64fca8630b30f673cc72133ff6232f1af63
usermuser <lwrdstv@gmail.com>
2019-07-16 23:17:07 +0500

    added TODO to Readme.rst

--- superhotel/timetable/views.py
+++ superhotel/timetable/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from .models import Room, DateItem #, Status
+from .models import Room, DateItem
 
 import datetime
+
 
 def get_days(days_qty=2, date=None):
     one_day = datetime.timedelta(1) # we will add one day in cycle
     result = []
 
-    if date == None:
+    if date is None:
         date = datetime.date.today()
         result.append(str(date))
 
@@ -16,16 +17,6 @@ def get_days(days_qty=2, date=None):
         result.append(str(date))
 
     return result   # ['2019-07-11', '2019-07-12', '2019-07-13']
-
-
-# print(get_days(days_qty=1))
-
-# all_dates = DateItem.objects.all().order_by('date_item')
-# all_rooms = Room.objects.order_by('order')
-
-# for room in all_rooms:
-#     room_dates = all_dates.filter(room_id=room.id)
-#     print(room_dates)
 
 
 def show_table(request):
@@ -46,7 +37,7 @@ def show_table(request):
 
 def show_week(request, start_date=None):
     title = 'Timetable'
-    if start_date == None:
+    if start_date is None:
         start_date = datetime.date.today()
         print(type(start_date), start_date)
     else:
@@ -54,8 +45,6 @@ def show_week(request, start_date=None):
 
     rooms = Room.objects.order_by('order')
     dates = DateItem.objects.order_by('date_item').filter(date_item__gte=start_date)[:3]
-    # timetable = dates.filter(room_id=rooms[0].id).order_by('date_item')[:2]
-    # print(timetable)
 
     ctx = {
         'title': title,
